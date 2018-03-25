709fb90840fb304308116d10a833629c865c01f8
Georgi Stoyanov <g.stoyanov45@gmail.com>
2018-03-25 18:51:33 +0300

    Exercise done

---
+++ 01.Recursion/Exercise/ExerciseSolutions/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseSolutions
+{
+    class Program
+    {
+        //Reversing printing array
+        private static void ReverseArray(string[] array, int index)
+        {
+            if (index < 0)
+            {
+                return;
+            }
+            Console.Write(array[index] + ' ');
+            ReverseArray(array, index - 1);
+        }
+
+        //Simulation 'n' numbers of loops and printing combinations from 1 to 'loops size'
+        private static void SimulateNestedLoops(int loops, int[] arr, int n)
+        {
+            if (loops == n)
+            {
+                Console.WriteLine(string.Join(" ", arr));
+                return;
+
+            }
+            else
+            {
+                for (int i = 1; i <= loops; i++)
+                {
+                    arr[n] = i;
+                    SimulateNestedLoops(loops, arr, n + 1);
+                }
+            }
+
+        }
+
+        //Get all combinations only once a.k.a if we get '1 3' we should skip '3 1'
+        private static void CombinationsWithRepetitions(int[] arr, int maxVal, int index = 0, int num = 1)
+        {
+            if (index >= arr.Length)
+            {
+                Console.WriteLine(string.Join(" ", arr));
+                return;
+            }
+
+            for (int i = num; i <= maxVal; i++)
+            {
+                arr[index] = i;
+                CombinationsWithRepetitions(arr, maxVal, index + 1, i);
+            }
+        }
+
+        //Get all non-duplicate combinations a.k.a skip '1 1' , '2 2' etc. 
+        private static void CombinationsWithoutRepetitions(int[] arr, int maxValue, int index = 0, int num = 1)
+        {
+            if (index >= arr.Length)
+            {
+                Console.WriteLine(string.Join(" ", arr));
+                return;
+            }
+
+            for (int i = num; i <= maxValue; i++)
+            {
+                arr[index] = i;
+                CombinationsWithoutRepetitions(arr, maxValue, index + 1, i + 1);
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+           
+        }
+    }
+}
---
+++ 01.Recursion/Exercise/Towers of Hanoi/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    private static Stack<int> source;
+    private static Stack<int> destination = new Stack<int>();
+    private static Stack<int> spare = new Stack<int>();
+    private static int step = 0;
+
+    static void Main(string[] args)
+    {
+        int disks = int.Parse(Console.ReadLine());
+        source = new Stack<int>(Enumerable.Range(1, disks).Reverse());
+        Print();
+        MoveDisks(disks, source, destination, spare);
+    }
+
+    private static void MoveDisks(int bottomDisk, Stack<int> source, Stack<int> destination, Stack<int> spare)
+    {
+        if (bottomDisk == 1)
+        {
+            step++;
+            destination.Push(source.Pop());
+            Console.WriteLine($"Step #{step}: Moved disk {bottomDisk}");
+            Print();
+        }
+        else
+        {
+            MoveDisks(bottomDisk - 1, source, spare, destination);
+            step++;
+            destination.Push(source.Pop());
+            Console.WriteLine($"Step #{step}: Moved disk {bottomDisk}");
+            Print();
+            MoveDisks(bottomDisk - 1, spare, destination, source);
+        }
+    }
+
+    private static void Print()
+    {
+        Console.WriteLine($"Source: {String.Join(", ", source.Reverse())}");
+        Console.WriteLine($"Destination: {String.Join(", ", destination.Reverse())}");
+        Console.WriteLine($"Spare: {String.Join(", ", spare.Reverse())}");
+        Console.WriteLine();
+    }
+}
